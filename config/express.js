7f012edb6ef67794fcf050e1fe5e435dab149ee3
Soul Evans <soulevans07@gmail.com>
2019-05-12 18:04:48 +0200

    initial commit with basic node boilerplate

---
+++ config/express.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const _ = require('lodash');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const { publicPath } = require('./vars');
+const { routesPath } = require('./vars');
+const { logs } = require('./vars');
+
+const app = express();
+
+app.use(morgan(logs));
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '5mb' }));
+
+app.use(helmet());
+app.use('/static', express.static(publicPath));
+
+const router = express.Router();
+const routes = require('require-all')({
+  dirname: routesPath,
+  filter: /.+\.js$/
+});
+
+_.mapValues(routes, (value, key) => {
+  const path = key.replace('.js', '');
+  router.use('/' + path, value);
+});
+
+router.use('/', function(req, res){
+	res.send("Up and running!");
+})
+
+app.use(router);
+
+module.exports = app;
